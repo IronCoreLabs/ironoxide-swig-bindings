1ae63439cdb05ba5d5c2a913656c295ef63fa8ed
Murph Murphy <murph@clurictec.com>
2019-05-30 14:35:47 -0600

    Make libs go to github when tags are built. (#19)
    Also updates rust to 1.35.0

---
+++ .travis_scripts/build-release.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# JAVA_HOME isn't set on OSX for some reason, so manually set it
+if [ -z "$JAVA_HOME" ]; then
+    export JAVA_HOME=$(/usr/libexec/java_home)
+fi
+
+cargo build --release
+
+# Copy out just the library to upload. In order to just grab the .dylib or .so we just use extglob, allowing use of |.
+# We did this instead of 2 cp lines so we could leave -e enabled and it would succeed as long as we have at least one of them.
+mkdir release_artifacts
+shopt -s extglob
+cp target/release/libironoxide_java.*(so|dylib) release_artifacts/
+shopt -u extglob
