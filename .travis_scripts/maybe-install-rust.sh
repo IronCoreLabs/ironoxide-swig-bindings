62bbb84bfbeb3b764ba653d3e89d8f4ef62d1ce7
Chris Jones <chris.jones@ironcorelabs.com>
2019-07-22 15:47:21 -0600

    travis: Create the cargo soft links whether rustc is installed or not.

--- .travis_scripts/maybe-install-rust.sh
+++ .travis_scripts/maybe-install-rust.sh
@@ -4,14 +4,10 @@
 
 set -ex
 
-if ${HOME}/.cargo/bin/rustc --version 2> /dev/null ; then
-    exit 0
+if ! ${HOME}/.cargo/bin/rustc --version 2> /dev/null ; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 fi
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # This hack lets us run rustc from .travis.yml when the build is inside Docker.
 ln -s ${HOME}/.cargo/bin/* /usr/local/bin
-
-echo $PATH
-ls -l /usr/local/bin/rust*
