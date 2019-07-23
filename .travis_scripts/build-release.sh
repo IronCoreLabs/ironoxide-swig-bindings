62c7386046e88b2d26980ac0d5d4024a79da7174
Chris Jones <chris.jones@ironcorelabs.com>
2019-07-23 13:39:26 -0600

    travis: Prepare the release script and debug.

--- .travis_scripts/build-release.sh
+++ .travis_scripts/build-release.sh
@@ -3,6 +3,11 @@
 set -e
 set -x
 
+# Set JAVA_HOME inside our CentOS Docker container.
+if [ -z "${JAVA_HOME}" -a -e /usr/lib/jvm/java-openjdk ] ; then
+    export JAVA_HOME=/usr/lib/jvm/java-openjdk
+fi
+
 # JAVA_HOME isn't set on OSX for some reason, so manually set it
 if [ -z "$JAVA_HOME" ]; then
     export JAVA_HOME=$(/usr/libexec/java_home)
