fdfa7d52695d022682c1e4626ddbec46501e42bd
Chris Jones <chris.jones@ironcorelabs.com>
2019-07-23 13:04:39 -0600

    travis: Install clang for the CentOS container.

--- .travis_scripts/maybe-start-docker.sh
+++ .travis_scripts/maybe-start-docker.sh
@@ -11,4 +11,4 @@ fi
 docker run --detach --name target -v $(pwd):/src -v ${HOME}:/root -w /src ${IMAGE} sleep 999999999
 
 docker exec target yum group install -y "Development Tools"
-docker exec target yum install -y java-sdk openssl-devel
+docker exec target yum install -y java-sdk openssl-devel clang
