85bcbb5c702a40da4cabb579399d03f67cf5422b
Chris Jones <chris.jones@ironcorelabs.com>
2019-07-23 10:26:11 -0600

    travis: Set up Java for the CentOS container.

--- .travis_scripts/cross-test.sh
+++ .travis_scripts/cross-test.sh
@@ -3,10 +3,17 @@
 set -e
 set -x
 
+# Set JAVA_HOME inside our CentOS Docker container.
+if [ -z "${JAVA_HOME}" -a -e /usr/lib/jvm/jre-1.8.0-openjdk ] ; then
+    export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
+fi
+
 # JAVA_HOME isn't set on OSX for some reason, so manually set it
 if [ -z "$JAVA_HOME" ]; then
     export JAVA_HOME=$(/usr/libexec/java_home)
 fi
+
+${JAVA_HOME}/bin/java -version
 
 cargo fmt -- --check
 cargo b
--- .travis_scripts/maybe-start-docker.sh
+++ .travis_scripts/maybe-start-docker.sh
@@ -11,3 +11,4 @@ fi
 docker run --detach --name target -v $(pwd):/src -v ${HOME}:/root -w /src ${IMAGE} sleep 999999999
 
 docker exec target yum group install -y "Development Tools"
+docker exec target yum install -y java-1.8.0-openjdk
