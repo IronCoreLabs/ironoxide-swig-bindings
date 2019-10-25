4872815059f47b1dbef0d158516e04db17184701
Chris Jones <chris.jones@ironcorelabs.com>
2019-10-25 11:07:30 -0600

    travis: Run CentOS commands in a different order.

--- .travis_scripts/maybe-start-docker.sh
+++ .travis_scripts/maybe-start-docker.sh
@@ -10,5 +10,6 @@ fi
 
 docker run --detach --name target -v "$(pwd)":/src -w /src -e IMAGE="${IMAGE}" centos:"${IMAGE}" sleep 999999999
 
-docker exec target yum group install -y "Development Tools"
+# Note that yum will fail if these are run in the reversed order:
 docker exec target yum install -y java-sdk openssl-devel clang
+docker exec target yum group install -y "Development Tools"
