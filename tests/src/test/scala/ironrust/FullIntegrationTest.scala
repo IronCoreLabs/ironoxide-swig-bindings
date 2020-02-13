31099d69fb9ff536b03a5cb66541dab547378788
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2020-02-13 15:09:55 -0700

    Added default constructors

--- tests/src/test/scala/ironrust/FullIntegrationTest.scala
+++ tests/src/test/scala/ironrust/FullIntegrationTest.scala
@@ -26,14 +26,14 @@ class FullIntegrationTest extends DudeSuite with CancelAfterFailure {
   var validGroupId: GroupId = null
   var validDocumentId: DocumentId = null
 
-  val defaultPolicyCaching = new PolicyCachingConfig(100)
+  val defaultPolicyCaching = new PolicyCachingConfig()
 
   val shortTimeout = Duration.from_millis(5)
   val defaultTimeout = Duration.from_secs(30)
   val longTimeout = Duration.from_secs(30)
 
   val shortConfig = new IronOxideConfig(defaultPolicyCaching, shortTimeout)
-  val defaultConfig = new IronOxideConfig(defaultPolicyCaching, defaultTimeout)
+  val defaultConfig = new IronOxideConfig()
 
   /**
     * Convenience function to create a new DeviceContext instance from the stored off components we need. Takes the
