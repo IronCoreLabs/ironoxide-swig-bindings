b04c5f9d2233f09d5f3b2320b584ffb6c57eda95
Clint Frederickson <clint.frederickson@ironcorelabs.com>
2020-03-25 10:22:41 -0600

    [#103] Add support for building an androind .aar package

---
+++ android/build.sh
@@ -0,0 +1,24 @@
+
+rm -rf ironoxide-android/src/main/java
+rm -rf ironoxide-android/src/main/jniLibs/*
+cd ../
+# sometimes this will be uncessary, but until we solve the duplicate ironoxide-android dirs in the target dir, probably safer to leave this here
+cargo clean
+# --lto currently isn't working. Need to look into this
+cross rustc --target i686-linux-android --release -p ironoxide-android
+cross rustc --target aarch64-linux-android --release -p ironoxide-android
+#cross rustc --target aarch64-linux-android --release -- -C lto
+
+# how can we handle multiple ironoxide-android directories at this level?
+# Can we make cargo only ever generate one? Can we remove any old ones we build again?
+# otherwise we will need to detect this cause and fail here
+cp -r target/i686-linux-android/release/build/ironoxide-android*/out/java android/ironoxide-android/src/main/
+#cp -r java/com ../ironoxide-android/src/main/java/
+mkdir android/ironoxide-android/src/main/jniLibs/x86/
+cp -r target/i686-linux-android/release/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/x86/
+mkdir android/ironoxide-android/src/main/jniLibs/arm64-v8a/
+cp -r target/i686-linux-android/release/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/arm64-v8a/
+
+cd android/
+./gradlew build
+ls ironoxide-android/build/outputs/aar
