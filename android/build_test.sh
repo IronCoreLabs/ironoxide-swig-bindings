f5600e3a761fc92c45642311b292e706e6c5eef6
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2020-03-30 17:14:36 -0600

    change release to debug for test

--- android/build_test.sh
+++ android/build_test.sh
@@ -9,7 +9,7 @@ cp -r target/i686-linux-android/debug/build/ironoxide-android*/out/java android/
 mkdir -p android/ironoxide-android/src/main/jniLibs/x86/
 cp -r target/i686-linux-android/debug/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/x86/
 mkdir -p android/ironoxide-android/src/main/jniLibs/x86_64/
-cp -r target/x86_64-linux-android/release/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/x86_64/
+cp -r target/x86_64-linux-android/debug/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/x86_64/
 mkdir -p android/ironoxide-android/src/main/jniLibs/arm64-v8a/
 cp -r target/aarch64-linux-android/debug/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/arm64-v8a/
 
