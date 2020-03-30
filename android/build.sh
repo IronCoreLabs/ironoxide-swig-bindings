00f1d29059da341a04159833fcdf2ca0e0660474
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2020-03-30 11:20:17 -0600

    change release to debug for testing

--- android/build.sh
+++ android/build.sh
@@ -9,11 +9,11 @@ cross build --target aarch64-linux-android -p ironoxide-android
 # how can we handle multiple ironoxide-android directories at this level?
 # Can we make cargo only ever generate one? Can we remove any old ones we build again?
 # otherwise we will need to detect this cause and fail here
-cp -r target/i686-linux-android/release/build/ironoxide-android*/out/java android/ironoxide-android/src/main/
+cp -r target/i686-linux-android/debug/build/ironoxide-android*/out/java android/ironoxide-android/src/main/
 mkdir -p android/ironoxide-android/src/main/jniLibs/x86/
-cp -r target/i686-linux-android/release/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/x86/
+cp -r target/i686-linux-android/debug/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/x86/
 mkdir -p android/ironoxide-android/src/main/jniLibs/arm64-v8a/
-cp -r target/aarch64-linux-android/release/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/arm64-v8a/
+cp -r target/aarch64-linux-android/debug/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/arm64-v8a/
 
 cd android/
 ./gradlew build
