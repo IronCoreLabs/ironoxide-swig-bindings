7c9218fa224484c1a23eb5436ab75886d0e1cd1b
Craig Colegrove <34786857+giarc3@users.noreply.github.com>
2020-04-03 14:33:44 -0600

    Add Android tests (#110)

--- android/build.sh
+++ android/build.sh
@@ -2,9 +2,10 @@
 rm -rf ironoxide-android/src/main/java
 rm -rf ironoxide-android/src/main/jniLibs/*
 cd ../
-# sometimes this will be uncessary, but until we solve the duplicate ironoxide-android dirs in the target dir, probably safer to leave this here
+# sometimes this will be unnecessary, but until we solve the duplicate ironoxide-android dirs in the target dir, probably safer to leave this here
 cargo clean
 cross rustc --target i686-linux-android --release -p ironoxide-android -- -C lto
+cross rustc --target x86_64-linux-android --release -p ironoxide-android -- -C lto
 cross rustc --target aarch64-linux-android --release -p ironoxide-android -- -C lto
 
 # how can we handle multiple ironoxide-android directories at this level?
@@ -13,6 +14,8 @@ cross rustc --target aarch64-linux-android --release -p ironoxide-android -- -C 
 cp -r target/i686-linux-android/release/build/ironoxide-android*/out/java android/ironoxide-android/src/main/
 mkdir -p android/ironoxide-android/src/main/jniLibs/x86/
 cp -r target/i686-linux-android/release/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/x86/
+mkdir -p android/ironoxide-android/src/main/jniLibs/x86_64/
+cp -r target/x86_64-linux-android/release/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/x86_64/
 mkdir -p android/ironoxide-android/src/main/jniLibs/arm64-v8a/
 cp -r target/aarch64-linux-android/release/libironoxide_android.so android/ironoxide-android/src/main/jniLibs/arm64-v8a/
 
---
+++ android/ironoxide-android/src/androidTest/java/com/ironcorelabs/sdk/FullIntegrationTest.java
@@ -0,0 +1,77 @@
+package com.ironcorelabs.sdk;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import java.util.UUID;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class FullIntegrationTest {
+    DeviceContext deviceContext;
+
+    public FullIntegrationTest() throws Exception {
+        System.loadLibrary("ironoxide_android");
+        final java.io.InputStream in = FullIntegrationTest.class.getClassLoader()
+                .getResourceAsStream("deviceContext.json");
+        final java.util.Scanner s = new java.util.Scanner(in).useDelimiter("\\A");
+        final String deviceString = s.hasNext() ? s.next() : "";
+        deviceContext = DeviceContext.fromJsonString(deviceString);
+    }
+
+    @Test
+    public void roundtripData() throws Exception {
+        final String data = "Test 123";
+        final IronOxide io = IronOxide.initialize(deviceContext, new IronOxideConfig());
+        final DocumentEncryptResult encryptResult = io.documentEncrypt(data.getBytes(), new DocumentEncryptOpts());
+        final DocumentDecryptResult decryptResult = io.documentDecrypt(encryptResult.getEncryptedData());
+        final String decryptedData = new String(decryptResult.getDecryptedData());
+        assertEquals(data, decryptedData);
+    }
+
+    @Test
+    public void roundtripDataUnmanaged() throws Exception {
+        final String data = "Test 123";
+        final IronOxide io = IronOxide.initialize(deviceContext, new IronOxideConfig());
+        final DocumentEncryptUnmanagedResult encryptResult = io.advancedDocumentEncryptUnmanaged(data.getBytes(),
+                new DocumentEncryptOpts());
+        assertEquals(encryptResult.getId().getId().length(), 32);
+
+        final DocumentDecryptUnmanagedResult decryptResult = io
+                .advancedDocumentDecryptUnmanaged(encryptResult.getEncryptedData(), encryptResult.getEncryptedDeks());
+        assertEquals(encryptResult.getId(), decryptResult.getId());
+
+        final String decryptedData = new String(decryptResult.getDecryptedData());
+        assertEquals(data, decryptedData);
+    }
+
+    @Test
+    public void timeoutInitialize() throws Exception {
+        try {
+            final IronOxide io = IronOxide.initialize(deviceContext,
+                    new IronOxideConfig(new PolicyCachingConfig(), Duration.fromMillis(5)));
+            org.junit.Assert.fail("Should have failed on timeout");
+        } catch (final Exception e) {
+            assertTrue("Incorrect exception: " + e.getMessage(), e.getMessage().contains("timed out"));
+        }
+    }
+
+    @Test
+    public void groupCreate() throws Exception {
+        final IronOxide io = IronOxide.initialize(deviceContext, new IronOxideConfig());
+        final GroupId groupId = GroupId.validate(UUID.randomUUID().toString());
+        final GroupCreateOpts opts = new GroupCreateOpts(groupId, null, true, true, null, new UserId[0], new UserId[0],
+                false);
+        final GroupCreateResult result = io.groupCreate(opts);
+        assertEquals(result.getId(), groupId);
+        assertEquals(result.getCreated(), result.getLastUpdated());
+    }
+
+    @Test
+    public void documentList() throws Exception {
+        final IronOxide io = IronOxide.initialize(deviceContext, new IronOxideConfig());
+        final DocumentListResult result = io.documentList();
+        assertTrue("There should have been some encrypted documents.", result.getResult().length > 0);
+    }
+
+}
