df50f2c8535ae0d064f44489ccbc91e37db7bd3d
Colt Frederickson <coltfred@gmail.com>
2020-04-22 11:52:02 -0600

    Add a few more tests

--- cpp/test/test.cpp
+++ cpp/test/test.cpp
@@ -17,6 +17,7 @@
 #include "GroupUserList_impl.hpp"
 #include "GroupId_impl.hpp"
 #include "GroupName_impl.hpp"
+#include "GroupListResult_impl.hpp"
 using namespace sdk;
 
 template <class T>
@@ -48,24 +49,26 @@ std::string vec_to_string(RustVeci8 a)
     return std::string(reinterpret_cast<char const *>(&a[0]), a.size());
 }
 
-std::string random_string(size_t length)
+std::string random_string(std::size_t length)
 {
-    auto randchar = []() -> char {
-        const char charset[] =
-            "0123456789"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            "abcdefghijklmnopqrstuvwxyz";
-        const size_t max_index = (sizeof(charset) - 1);
-        return charset[rand() % max_index];
-    };
-    std::string str(length, 0);
-    std::generate_n(str.begin(), length, randchar);
-    return str;
-}
+    const std::string CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<> distribution(0, CHARACTERS.size() - 1);
+
+    std::string random_string;
+
+    for (std::size_t i = 0; i < length; ++i)
+    {
+        random_string += CHARACTERS[distribution(generator)];
+    }
+
+    return random_string;
+}
 std::string random_id()
 {
-    return random_string(90);
+    return random_string(94);
 }
 
 auto deviceContextString = "{\"accountId\":\"abcABC012_.$#|@/:;=+'-91e078f0-a60c-4251-8652-dd498c07a8f4\",\"segmentId\":1825,\"signingPrivateKey\":\"uKHa70uwLVG3IU7XodT2kla/PuC/En8PkRCjMMc9ZE7HFrOV+g0vOwATp/CiXp65mVas0K6TSl/RaxDGlcmsnA==\",\"devicePrivateKey\":\"YZRlDSkM+JxxSXCtWCVK693qfhNqcbhaPrtHs92uD4w=\"}";
@@ -127,11 +130,41 @@ void group_create_passing_args(void)
     TEST_MSG("Group create failed.");
 }
 
+void group_create_passing_nulls(void)
+{
+    DeviceContext d = unwrap(DeviceContext::fromJsonString(deviceContextString));
+    IronOxide sdk = unwrap(IronOxide::initialize(d, IronOxideConfig()));
+    auto group_create_result = unwrap(sdk.groupCreate(GroupCreateOpts(nullptr, nullptr, true, true, nullptr, RustForeignVecUserId(), RustForeignVecUserId(), false)));
+    TEST_CHECK(group_create_result.getMemberList().getList().as_slice().size() == 1);
+    TEST_MSG("Group create failed.");
+}
+
+void group_list(void)
+{
+    DeviceContext d = unwrap(DeviceContext::fromJsonString(deviceContextString));
+    IronOxide sdk = unwrap(IronOxide::initialize(d, IronOxideConfig()));
+    auto group_list_result = unwrap(sdk.groupList());
+    TEST_CHECK(group_list_result.getResult().as_slice().size() > 1);
+    TEST_MSG("Group list failed.");
+}
+
+void document_encrypt(void)
+{
+    DeviceContext d = unwrap(DeviceContext::fromJsonString(deviceContextString));
+    IronOxide sdk = unwrap(IronOxide::initialize(d, IronOxideConfig()));
+    auto encrypt_result = unwrap(sdk.documentEncrypt(string_to_slice("foo"), DocumentEncryptOpts()));
+    TEST_CHECK(!encrypt_result.getName().has_value());
+    TEST_MSG("Document encrypt failed.");
+}
+
 TEST_LIST = {
-    // {"test_user_id", test_user_id},
-    // {"test_user_id_error", test_user_id_error},
-    // {"encrypt_decrypt_roundtrip", encrypt_decrypt_roundtrip},
-    // {"group_name", group_name},
-    // {"group_create_default", group_create_default},
+    {"test_user_id", test_user_id},
+    {"test_user_id_error", test_user_id_error},
+    {"encrypt_decrypt_roundtrip", encrypt_decrypt_roundtrip},
+    {"group_name", group_name},
+    {"group_create_default", group_create_default},
     {"group_create_passing_args", group_create_passing_args},
+    {"group_create_passing_nulls", group_create_passing_nulls},
+    {"group_list", group_list},
+    {"document_encrypt", document_encrypt},
     {NULL, NULL}};
