6e2642940cf814797e8ddaa7e79e6549d8b316a6
Craig Colegrove <34786857+giarc3@users.noreply.github.com>
2020-05-19 10:40:51 -0600

    Overhaul ironoxide-java tests (#129)

---
+++ java/tests/src/test/scala/ironoxide/SearchTests.scala
@@ -0,0 +1,32 @@
+package ironoxide
+
+import scala.util.Try
+import com.ironcorelabs.sdk._
+
+class SearchTests extends TestSuite {
+  "Tokenize" should {
+    "tokenize data and a query" in {
+      val searchQuery = "ironcore labs"
+      val groupId = Try(primarySdk.groupCreate(new GroupCreateOpts)).toEither.value.getId
+      val ebis = Try(primarySdk.createBlindIndex(groupId)).toEither.value
+      val bis = Try(ebis.initializeSearch(primarySdk)).toEither.value
+      val queryTokens = Try(bis.tokenizeQuery(searchQuery, "")).toEither.value
+      val dataTokens = Try(bis.tokenizeData(searchQuery, "")).toEither.value
+      queryTokens.length shouldBe 8
+      dataTokens.length should be > 8
+      queryTokens.toSet.subsetOf(dataTokens.toSet) shouldBe true
+    }
+    "tokenize with partition" in {
+      val searchQuery = "ironcore labs"
+      val groupId = Try(primarySdk.groupCreate(new GroupCreateOpts)).toEither.value.getId
+      val ebis = Try(primarySdk.createBlindIndex(groupId)).toEither.value
+      val bis = Try(ebis.initializeSearch(primarySdk)).toEither.value
+      val queryTokens = Try(bis.tokenizeQuery(searchQuery, "")).toEither.value
+      val queryTokens2 = Try(bis.tokenizeQuery(searchQuery, "foo")).toEither.value
+      queryTokens.length shouldBe 8
+      queryTokens2.length shouldBe 8
+      queryTokens should not be queryTokens2
+
+    }
+  }
+}
---
+++ java/tests/src/test/scala/ironoxide/package.scala
@@ -0,0 +1,53 @@
+import com.ironcorelabs.sdk._
+import scala.util.Try
+import cats.scalatest.EitherValues
+import pdi.jwt.{Jwt, JwtClaim, JwtAlgorithm};
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+package object ironoxide extends EitherValues {
+  def createUserAndDevice(maybeUserId: Option[UserId] = None): DeviceContext = {
+    val userId = maybeUserId.getOrElse(UserId.validate(java.util.UUID.randomUUID.toString))
+    val jwt = generateValidJwt(userId.getId)
+    IronOxide.userCreate(jwt, testUsersPassword, new UserCreateOpts(true), null)
+    val dar = IronOxide.generateNewDevice(jwt, testUsersPassword, new DeviceCreateOpts, null)
+    new DeviceContext(dar)
+  }
+
+  def generateValidJwt(accountId: String = java.util.UUID.randomUUID.toString, expiresInSec: Long = 120) = {
+    java.security.Security.addProvider(new BouncyCastleProvider)
+
+    val projectId = 431
+    val segmentId = "ironoxide-java"
+    val iakId = 597
+    val key =
+      "MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgNdz7Kffzhn1PnAy1XI2YFUEnDC9TWbzctzIGvWqNv82hRANCAAQG3CAlUWHmBLRRkbwB4zSierd8BRbbyUlczZoNOiXKwbia9CZ9DJ/Dh72c8yFNNVr+hKdvV4Vhj2MQGbMF7Qo3"
+    val currentTime = System.currentTimeMillis() / 1000
+    val claim = JwtClaim({ raw"""{"pid": $projectId,"sid": "$segmentId","kid": $iakId}""" })
+      .about(accountId)
+      .issuedAt(currentTime)
+      .expiresAt(currentTime + expiresInSec)
+    Jwt.encode(claim, key, JwtAlgorithm.ES256)
+  }
+
+  val testUsersPassword = java.util.UUID.randomUUID.toString
+
+  lazy val (primaryUser, primaryUserDevice, primarySdk) = {
+    val primaryUser = Try(UserId.validate(java.util.UUID.randomUUID.toString)).toEither.value
+    val jwt = generateValidJwt(primaryUser.getId)
+    IronOxide.userCreate(jwt, testUsersPassword, new UserCreateOpts, null)
+    val dar = Try(IronOxide.generateNewDevice(jwt, testUsersPassword, new DeviceCreateOpts, null)).toEither.value
+    val primaryUserDevice = new DeviceContext(dar)
+    val primarySdk = IronOxide.initialize(primaryUserDevice, new IronOxideConfig)
+    (primaryUser, primaryUserDevice, primarySdk)
+  }
+
+  lazy val (secondaryUser, secondaryUserDevice, secondarySdk) = {
+    val secondaryUser = Try(UserId.validate(java.util.UUID.randomUUID.toString)).toEither.value
+    val jwt = generateValidJwt(secondaryUser.getId)
+    IronOxide.userCreate(jwt, testUsersPassword, new UserCreateOpts, null)
+    val dar = Try(IronOxide.generateNewDevice(jwt, testUsersPassword, new DeviceCreateOpts, null)).toEither.value
+    val secondaryUserDevice = new DeviceContext(dar)
+    val secondarySdk = IronOxide.initialize(secondaryUserDevice, new IronOxideConfig)
+    (secondaryUser, secondaryUserDevice, secondarySdk)
+  }
+}
