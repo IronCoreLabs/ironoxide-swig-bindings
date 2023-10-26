9a0c37aab15b404f5577a6ae763cc8e2b2c1756b
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2023-10-26 10:21:35 -0700

    Baseline

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "IronOxide Swig Bindings";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rusttoolchain =
+          pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        # nix develop
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs;
+            [
+              rusttoolchain
+              pkg-config
+              pkgs.openssl
+              openjdk17
+            ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin
+              [ pkgs.darwin.apple_sdk.frameworks.SystemConfiguration ];
+        };
+
+      });
+}
