8c368f2810f95ba8071a8af72a46fd8ae2cc76d1
Craig Colegrove <craig.colegrove@ironcorelabs.com>
2023-10-26 11:01:26 -0700

    Mistake

--- android/build.sh
+++ android/build.sh
@@ -12,7 +12,7 @@ rm -rf ironoxide-android/src/main/jniLibs/x86_64
 cd ../
 
 cargo clean
-cross build -p ironoxide-android --target aarch64-linux-android 
+cross build -p ironoxide-android --target x86_64-linux-android 
 
 cp -r target/x86_64-linux-android/debug/build/ironoxide-android*/out/java android/ironoxide-android/src/main/
 mkdir -p android/ironoxide-android/src/main/jniLibs/x86_64/
