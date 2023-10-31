7ee24518e9845fa997bf45b3854f7d5fc38a0b31
Craig Colegrove <34786857+giarc3@users.noreply.github.com>
2023-10-31 07:51:50 -0700

    CI fixes (#220)

--- java/tests/src/test/scala/ironoxide/DocumentTests.scala
+++ java/tests/src/test/scala/ironoxide/DocumentTests.scala
@@ -18,15 +18,15 @@ class DocumentTests extends TestSuite {
     "roundtrip bytes" in {
       val bytes = Array(2, 3).map(_.toByte)
       val encryptResult = Try(primarySdk.documentEncrypt(bytes, new DocumentEncryptOpts)).toEither.value
+      val currentTime = java.lang.System.currentTimeMillis
       encryptResult.getName.isEmpty shouldBe true
       encryptResult.getErrors.getUsers.isEmpty shouldBe true
       encryptResult.getErrors.getGroups.isEmpty shouldBe true
       encryptResult.getChanged.getUsers.length shouldBe 1
       val createdTime = encryptResult.getCreated().toInstant().toEpochMilli
       val updatedTime = encryptResult.getLastUpdated().toInstant().toEpochMilli
-      val currentTime = java.lang.System.currentTimeMillis
-      (currentTime - createdTime) should be < 10000L // sanity check the document was created in the last 10 seconds
-      (currentTime - updatedTime) should be < 10000L // sanity check the document was last updated in the last 10 seconds
+      (currentTime - createdTime) should be < 20000L // sanity check the document was created in the last 20 seconds
+      (currentTime - updatedTime) should be < 20000L // sanity check the document was last updated in the last 20 seconds
 
       val decryptResult = Try(primarySdk.documentDecrypt(encryptResult.getEncryptedData)).toEither.value
       decryptResult.getDecryptedData shouldBe bytes
